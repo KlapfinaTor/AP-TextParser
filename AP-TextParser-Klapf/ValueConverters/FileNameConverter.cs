9bd3e2d9f9128cd118960ee2853cd017185a2c74
Stefan <stefan.klapf@gmail.com>
2022-06-29 12:04:32 +0200

    Refactored UI to look more modern and clean, Added a valueconverter for the filename

---
+++ AP-TextParser-Klapf/ValueConverters/FileNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.IO;
+
+namespace AP_TextParser_Klapf.ValueConverters
+{
+    public class FileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) //viewmodel ->view
+        {
+            
+            string rawPath = (string)value;
+            return  Path.GetFileNameWithoutExtension(rawPath);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) //view ->viewmmodel
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
